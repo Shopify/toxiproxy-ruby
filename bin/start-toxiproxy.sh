43df85cc3bb0a3ed9f159a8b95fe4e144a4fe5df
Jacob Wirth <xthexder@live.com>
2014-11-10 13:08:25 -0500

    Update toxiproxy binary

--- bin/start-toxiproxy.sh
+++ bin/start-toxiproxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-VERSION='fad1365c087d6ad53944c8201a6131cb'
+VERSION='9c4096dfa4028caf4de933cbc2edd88d'
 
 echo "[start toxiproxy]"
 curl --silent http://shopify-vagrant.s3.amazonaws.com/toxiproxy/toxiproxy-$VERSION -o ./bin/toxiproxy
