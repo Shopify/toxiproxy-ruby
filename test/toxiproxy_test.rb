3e24b7a4dce46d052b2a2aea4e73e69a9aa70e4f
Jacob Wirth <jacob.wirth@shopify.com>
2015-05-08 17:13:27 +0000

    Also test upstream changes

--- test/toxiproxy_test.rb
+++ test/toxiproxy_test.rb
@@ -303,7 +303,7 @@ class ToxiproxyTest < MiniTest::Unit::TestCase
     end
   end
 
-  def test_populate_creates_proxies_update
+  def test_populate_creates_proxies_update_listen
     proxies = [{
         name: "test_toxiproxy_populate1",
         upstream: "localhost:3306",
@@ -325,6 +325,29 @@ class ToxiproxyTest < MiniTest::Unit::TestCase
     proxies.each do |proxy|
       assert_proxy_available(proxy)
     end
+  end
+
+  def test_populate_creates_proxies_update_upstream
+    proxies = [{
+        name: "test_toxiproxy_populate1",
+        upstream: "localhost:3306",
+        listen: "localhost:22222",
+      },
+    ]
+
+    proxies = Toxiproxy.populate(proxies)
+
+    proxies = [{
+        name: "test_toxiproxy_populate1",
+        upstream: "localhost:3307",
+        listen: "localhost:22222",
+      },
+    ]
+
+    proxies2 = Toxiproxy.populate(proxies)
+
+    assert_equal proxies.first[:upstream], proxies2.first.upstream
+    assert_proxy_available(proxies2.first)
   end
 
   private
