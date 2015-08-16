30779d6f686fc922b87153dfd455d74a8587d37e
Ivan Povalyukhin <ivanpoval@gmail.com>
2015-08-15 22:21:33 -0700

    added default TOXIPROXY_LOG_DIR if CIRCLE_ARTIFACTS is not set so the bash script could be run on local env

--- bin/start-toxiproxy.sh
+++ bin/start-toxiproxy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
 VERSION='21a264cc75549c3ae837b606eb6e17ea'
+TOXIPROXY_LOG_DIR=${CIRCLE_ARTIFACTS:-'/tmp'}
 
 echo "[start toxiproxy]"
 curl --silent http://shopify-vagrant.s3.amazonaws.com/toxiproxy/toxiproxy-$VERSION -o ./bin/toxiproxy
 chmod +x ./bin/toxiproxy
-nohup bash -c "./bin/toxiproxy > ${CIRCLE_ARTIFACTS}/toxiproxy.log 2>&1 &"
+nohup bash -c "./bin/toxiproxy > ${TOXIPROXY_LOG_DIR}/toxiproxy.log 2>&1 &"
