a44448f64ad37b507c70095ea02a849fa7bdfa7f
Jacob Wirth <jacob@stripe.com>
2016-03-29 17:43:04 -0700

    Fix toxiproxy server download script

--- bin/start-toxiproxy.sh
+++ bin/start-toxiproxy.sh
@@ -4,6 +4,6 @@ VERSION='v2.0.0rc2'
 TOXIPROXY_LOG_DIR=${CIRCLE_ARTIFACTS:-'/tmp'}
 
 echo "[start toxiproxy]"
-curl --silent https://github.com/Shopify/toxiproxy/releases/download/$VERSION/toxiproxy-server-linux-amd64 -o ./bin/toxiproxy-server
+curl --silent -L https://github.com/Shopify/toxiproxy/releases/download/$VERSION/toxiproxy-server-linux-amd64 -o ./bin/toxiproxy-server
 chmod +x ./bin/toxiproxy-server
 nohup bash -c "./bin/toxiproxy-server > ${TOXIPROXY_LOG_DIR}/toxiproxy.log 2>&1 &"
